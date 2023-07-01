b0965d01b41198d52b3a10cd8713326461ad58bc
Giovanni Minari <80293487+GioLeiren@users.noreply.github.com>
2023-07-01 01:35:18 -0300

    Add files via upload

---
+++ static/js/Cadastro de Evento.js
@@ -0,0 +1,7 @@
+const myFile = document.getElementById('myFile');
+
+const fileChosen = document.getElementById('file-chosen');
+
+myFile.addEventListener('change', function(){
+  fileChosen.textContent = this.files[0].name
+})
